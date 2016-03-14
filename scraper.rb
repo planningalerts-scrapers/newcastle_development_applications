755f0cbb8c410a2747875f9a0931982a553dea6b
Henare Degan <henare.degan@gmail.com>
2016-03-14 20:41:35 +1100

    Comment out fetching bad data from ATDIS feed

--- scraper.rb
+++ scraper.rb
@@ -3,4 +3,5 @@ Bundler.require
 
 url = "http://da.ballina.nsw.gov.au/atdis/1.0"
 
-ATDISPlanningAlertsFeed.save(url)
+# ATDISPlanningAlertsFeed.save(url)
+raise "Addresses in this feed are broken, see https://github.com/planningalerts-scrapers/ballina_shire/commit/b344be524824800a93ea2b17ecd5042fab4f07b1"
