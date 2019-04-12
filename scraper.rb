785f0aff361cb82fdd3b18d4a8227b5272cef066
James Polley <jp@jamezpolley.com>
2019-04-12 14:17:45 +1000

    Provide Newcastle test URL

--- scraper.rb
+++ scraper.rb
@@ -1,6 +1,6 @@
 #!/usr/bin/env ruby
 Bundler.require
 
-url = "http://da.ballina.nsw.gov.au/atdis/1.0"
+url = "https://property.ncc.nsw.gov.au/T1PRTESTBAU/WebAppServices/ATDIS/atdis/1.0/"
 
 ATDISPlanningAlertsFeed.save(url)
