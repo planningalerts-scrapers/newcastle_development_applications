a9291ba47115cafb4ad4517ecdf44a2f5d426dc5
James Polley <jp@jamezpolley.com>
2019-04-12 14:21:04 +1000

    Update scraper.rb

--- scraper.rb
+++ scraper.rb
@@ -1,6 +1,6 @@
 #!/usr/bin/env ruby
 Bundler.require
 
-url = "https://property.ncc.nsw.gov.au/T1PRTESTBAU/WebAppServices/ATDIS/atdis/1.0/"
+url = "https://property.ncc.nsw.gov.au/T1PRTESTBAU/WebAppServices/ATDIS/atdis/1.0/applications.json"
 
 ATDISPlanningAlertsFeed.save(url)
