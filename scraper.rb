a8b771719c973f443964325855efb9ef8a8b7f8d
James Polley <jp@jamezpolley.com>
2019-07-05 13:09:43 +1000

    Update scraper.rb

--- scraper.rb
+++ scraper.rb
@@ -3,4 +3,4 @@ Bundler.require
 
 url = "https://property.ncc.nsw.gov.au/T1PRPROD/WebAppServices/ATDIS/atdis/1.0"
 
-ATDISPlanningAlertsFeed.save(url, timezone="Sydney", {lodgement_date_start: "2018-01-01", lodgement_end_date: "2018-03-01"})
+ATDISPlanningAlertsFeed.save(url, timezone="Sydney")
