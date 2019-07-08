2be4b4d6b03d51a8ded488b2ff82b998b79270ec
Matthew Landauer <mlandauer@gmail.com>
2019-07-08 17:00:53 +1000

    Just use the default date range where it gets the last 30 days.
    We don't want to hardcode the start date. List will otherwise get bigger 
    over time.

--- scraper.rb
+++ scraper.rb
@@ -3,4 +3,4 @@ Bundler.require
 
 url = "https://property.ncc.nsw.gov.au/T1PRPROD/WebAppServices/ATDIS/atdis/1.0"
 
-ATDISPlanningAlertsFeed.save(url, timezone="Sydney", lodgement_date_start: "2019-07-01")
+ATDISPlanningAlertsFeed.save(url, timezone="Sydney")
